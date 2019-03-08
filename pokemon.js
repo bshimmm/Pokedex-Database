b671d9bd5e0f23fed268ac6de9ec874de89bc280
Brian Shim <shimb@oregonstate.edu>
2019-03-08 15:19:05 -0500

    implemented delete function

--- pokemon.js
+++ pokemon.js
@@ -47,6 +47,55 @@ module.exports = function () {
       }
     })
   })
+//update pokemon
+/*
+  router.get('/update', function(req,res,next){
+    var mysql = req.app.get('mysql');
+    var context = {};
+    mysql.pool.query("SELECT * FROM Pokemon WHERE p_name=?", [req.body.p_name], function(error, result) {
+      if(error) {
+        next(error);
+        return;
+      }
+      if(result){
+        var curVals = result[0];
+        mysql.pool.query("UPDATE Pokemon SET p_catchrate=? WHERE p_name=?",
+        [req.body.p_catchrate || curVals.p_catchrate, req.body.p_name], function(error, result){
+            if(error){
+              next(error);
+              return;
+            }
+          mysql.pool.query("SELECT * FROM Pokemon", function(error, results, fields) {
+            if(error){
+              next(error);
+              return;
+            }
+            context.pokemon = results;
+            res.render('pokemon', context);
+          })
+        })
+      }
+    })
+  })
+  */
 
+
+/* This might be the source of my problem */
+  //delete pokemon
+  router.delete('/delete', function(req, res) {
+  var mysql = req.app.get('mysql'); 
+  var sql = "DELETE FROM pokemon WHERE p_id=?";
+  var inserts = [req.params.p_id];
+  sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+      if(error){
+       res.write(JSON.stringify(error)); 
+         res.status(400); 
+       res.end(); 
+      } else{
+        res.status(202).end();
+      }
+    })
+  })
+  
   return router;
 }();
